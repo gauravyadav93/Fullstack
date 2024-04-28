2e46dedc73398eeccc19fedf94f7043f10d35e5b
gauravyadav93 <gauravyyadav6@gmail.com>
2024-04-28 07:22:35 +0530

    Something new

---
+++ JavaScript Tutorial/demo.js
@@ -0,0 +1,11 @@
+console.log(5+4);
+console.log("This is JAvascript");
+
+function get_name(p1, p2, ...p3){
+    console.log(p1, p2, p3);
+}
+get_name(10, 20, 30, 40);
+
+let add = (p1, p2) =>{
+    return p1 + p2;
+}
